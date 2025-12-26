49d6153cad8606b72dfeb033f73f47934e6faa29
katiem120598 <katiem120598@gmail.com>
2025-12-25 23:15:23 -0500

    got day 3 working!!

---
+++ 12.25.2025/fshader.frag
@@ -0,0 +1,70 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define TAU 6.2831853071
+#define THICKNESS 0.2
+
+uniform vec2 u_resolution;
+uniform vec2 u_stored1;
+uniform vec2 u_stored2;
+uniform vec2 u_stored3;
+uniform vec2 u_stored4;
+uniform vec2 u_stored5;
+uniform vec3 u_col1;
+uniform vec3 u_col2;
+uniform vec3 u_col3;
+uniform vec3 u_col4;
+uniform vec3 u_col5;
+uniform float u_time1;
+uniform float u_time2;
+uniform float u_time3;
+uniform float u_time4;
+uniform float u_time5;
+
+void main() {
+    vec2 p = gl_FragCoord.xy; 
+    float d1 = distance(p, u_stored1);
+    float d2 = distance(p, u_stored2);
+    float d3 = distance(p, u_stored3);
+    float d4 = distance(p, u_stored4);
+    float d5 = distance(p, u_stored5);
+    float f1 = 10.0;
+    float f2 = 7.0;
+    float f3 = 13.0;
+    float r11 = 10.0*u_time1+45.0;
+    float r21 = 5.0*u_time2*u_time2+23.0;
+    float r31 = 5.0*u_time3*u_time3+60.0;
+    float r41 = u_time4*u_time4*u_time4+15.0;
+    float r51 = 10.0*u_time5 + 35.0;
+    float r12 = 10.0*u_time1+43.0;
+    float r22 = 3.0*u_time2*u_time2+22.0;
+    float r32 = 5.0*u_time3*u_time3+57.0;
+    float r42 = u_time4*u_time4+14.0;
+    float r52 = 10.0*u_time4 + 30.0;
+
+    // 1.0 inside the circle, 0.0 outside
+    float inside11 = step(r11, d1);
+    float inside12 = step(r12, d1);
+    float inside21 = step(r21, d2);
+    float inside22 = step(r22, d2);
+    float inside31 = step(r31, d3);
+    float inside32 = step(r32, d3);
+    float inside41 = step(r41, d4);
+    float inside42 = step(r42, d4);
+    float inside51 = step(r51, d5);
+    float inside52 = step(r52, d5);
+    
+    vec3 col1 = u_col1*(inside12-inside11);
+    vec3 col2 = u_col2*(inside22-inside21);
+    vec3 col3 = u_col3*(inside32-inside31);
+    vec3 col4 = u_col4*(inside42-inside41);
+    vec3 col5 = u_col5*(inside52-inside51);
+
+    // white outside, black inside
+    vec3 sum =col1+col2+col3+col4+col5;
+    vec4 clr = vec4(sum, 1.0);
+    
+
+    gl_FragColor = clr;
+}
---
+++ 12.25.2025/shader.js
@@ -0,0 +1,172 @@
+let myShader;
+let mic, fft;
+let micReady = false;
+let audioStarted = false;
+
+let storedind = 0;
+let stored1 = [0,0]
+let stored2 = [0,0.0]
+let stored3 = [0,0]
+let stored4 = [0,0]
+let stored5 = [0,0]
+let rvals = [0,0,0,0,0];
+let gvals = [0,0,0,0,0];
+let bvals = [0,0,0,0,0];
+let col1 = [0,0,0];
+let col2 = [0,0,0];
+let col3 = [0,0,0];
+let col4 = [0,0,0];
+let col5 = [0,0,0];
+let xvec = [0,0,0,0,0];
+let yvec = [0,0,0,0,0];
+
+let timevec = [0,0,0,0,0];
+let time1 = 0.0;
+let time2 = 0.0;
+let time3 = 0.0;
+let time4 = 0.0;
+let time5 = 0.0;
+
+let prevBass = 0.0;
+let prevMids = 0.0;
+let prevTreble = 0.0;
+
+let bassTrans = 0.0;
+let midsTrans = 0.0;
+let trebleTrans = 0.0;
+
+let bassThresh = 200.0;
+let midsThresh = 150.0;
+let trebleThresh = 90.0;
+
+let dimx, dimy;
+
+function preload() {
+  myShader = loadShader('vshader.vert', 'fshader.frag');
+}
+
+function setup() {
+  dimx = min(windowHeight-300, windowWidth-300);
+  dimy = dimx;
+  createCanvas(dimx,dimy,WEBGL);
+  pixelDensity(1);
+  shader(myShader);
+
+  if (!audioStarted) {
+    audioStarted = true;
+    userStartAudio()
+      .then(() => {
+        console.log("🔊 Audio context started");
+        startMic();
+      })
+      .catch(err => {
+        console.error("userStartAudio failed:", err);
+      });
+  }
+}
+
+function startMic() {
+  mic = new p5.AudioIn();
+  mic.start(
+    () => {
+      console.log("✅ Default mic started");
+      fft = new p5.FFT(0.8, 1024);
+      fft.setInput(mic);
+      mic.amp(1);
+      micReady = true;
+    },
+    err => {
+      console.error("Mic start error:", err);
+    }
+  );
+}
+
+function draw() {
+  if (micReady && fft) {
+    // force FFT so getEnergy is valid
+    fft.analyze();
+
+    const bass   = fft.getEnergy("bass");
+    const mids   = fft.getEnergy("mid");
+    const treble = fft.getEnergy("treble");
+
+    bassTrans = (prevBass<bassThresh && bass>=bassThresh)?1.0:0.0;
+    midsTrans = (prevMids<midsThresh && mids>=midsThresh)?1.0:0.0;
+    trebleTrans = (prevTreble<trebleThresh && treble>=trebleThresh)?1.0:0.0;
+
+
+
+    if (bassTrans){
+      xvec[storedind] = Math.random()*width;
+      yvec[storedind] = Math.random()*height;
+      rvals[storedind] = 225.0/255.0;
+      gvals[storedind] = 0.0;
+      bvals[storedind] = 0.0;
+      timevec[storedind] = millis()/1000.0;
+      storedind = (storedind + 1) % 5;
+    }
+
+    if (midsTrans){
+      xvec[storedind] = Math.random()*width;
+      yvec[storedind] = Math.random()*height;
+      rvals[storedind] = 0.0;
+      gvals[storedind] = 150.0/255.0;
+      bvals[storedind] = 50.0/255.0;
+      timevec[storedind] = millis()/1000.0;
+      storedind = (storedind + 1) % 5;
+    }
+
+    if (trebleTrans){
+      xvec[storedind] = Math.random()*width;
+      yvec[storedind] = Math.random()*height;
+      rvals[storedind] = 245.0/255.0;
+      gvals[storedind] = 245.0/255.0;
+      bvals[storedind] = 245.0/255.0;
+      timevec[storedind] = millis()/1000.0;
+      storedind = (storedind + 1) % 5;
+    }
+    prevBass = bass;
+    prevMids = mids;
+    prevTreble = treble;
+  }
+
+  stored1 = [xvec[0], yvec[0]];
+  stored2 = [xvec[1], yvec[1]];
+  stored3 = [xvec[2], yvec[2]];
+  stored4 = [xvec[3], yvec[3]];
+  stored5 = [xvec[4], yvec[4]];
+  col1 = [rvals[0], gvals[0], bvals[0]];
+  col2 = [rvals[1], gvals[1], bvals[1]];
+  col3 = [rvals[2], gvals[2], bvals[2]];
+  col4 = [rvals[3], gvals[3], bvals[3]];
+  col5 = [rvals[4], gvals[4], bvals[4]];
+  time1 = millis()/1000.0-timevec[0];
+  time2 = millis()/1000.0-timevec[1];
+  time3 = millis()/1000.0-timevec[2];
+  time4 = millis()/1000.0-timevec[3];
+  time5 = millis()/1000.0-timevec[4];
+  myShader.setUniform('u_resolution', [width, height]);
+  myShader.setUniform('u_stored1', stored1);
+  myShader.setUniform('u_stored2', stored2);
+  myShader.setUniform('u_stored3', stored3);
+  myShader.setUniform('u_stored4', stored4);
+  myShader.setUniform('u_stored5', stored5);
+  myShader.setUniform('u_col1', col1);
+  myShader.setUniform('u_col2', col2);
+  myShader.setUniform('u_col3', col3);
+  myShader.setUniform('u_col4', col4);
+  myShader.setUniform('u_col5', col5);
+  myShader.setUniform('u_time1', time1);
+  myShader.setUniform('u_time2', time2);
+  myShader.setUniform('u_time3', time3);
+  myShader.setUniform('u_time4', time4);
+  myShader.setUniform('u_time5', time5);
+
+  rect(0, 0, width, height);
+}
+
+function windowResized() {
+  dimx = min(windowHeight-300, windowWidth-300);
+  dimy = dimx;
+  resizeCanvas(dimx,dimy);
+}
